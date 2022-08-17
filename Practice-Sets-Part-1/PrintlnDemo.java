336a7cbc92e029855c724ff4afc41dc927be8091
chek2x <zlorenzo@mcm.edu.ph>
2022-08-17 21:07:47 +0800

    Added PrintlnDemo.java

---
+++ Practice-Sets-Part-1/PrintlnDemo.java
@@ -0,0 +1,21 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Object Oriented Programming (Paired)
+ */
+
+public class PrintlnDemo {
+    public static void main(String[] args) {
+        Ownership.Introduction();
+
+        System.out.println("This is a string of output");
+        System.out.println("A blank line follows this line");
+        System.out.println("");
+        System.out.println("A number follows this line");
+        System.out.println(762);
+        System.out.println("This is one number " + 45 +
+                " and another number " + 98);
+
+        System.out.println("\nThis is a concatenation of both string and integer: String and " + 157 + "\n");
+    }
+}
