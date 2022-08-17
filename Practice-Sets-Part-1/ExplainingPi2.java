b493bac2615cd5106871f4af1a190ee5cfd57d22
chek2x <zlorenzo@mcm.edu.ph>
2022-08-17 21:03:15 +0800

    Modified ExplainingPi2.java

--- Practice-Sets-Part-1/ExplainingPi2.java
+++ Practice-Sets-Part-1/ExplainingPi2.java
@@ -6,18 +6,20 @@
 
 public class ExplainingPi2 {
     public static void main(String[] args) {
+        Ownership.Introduction();
+
         System.out.println("Pi represents the ratio between");
         System.out.println("the diameter and circumference");
         System.out.println("of a circle");
         System.out.println(Math.PI);
 
         System.out.print("""
-                
+
                 This is a revision to conform to
                 what is to be learned from the activity.
                 The class name was renamed from
                 'ExplainingPi' to 'ExplainingPi2'
-                
-                """;);
+
+                """);
     }
 }
