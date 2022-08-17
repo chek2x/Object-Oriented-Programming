a82d74850084e94fda4682b11dd11c8f68e04f50
chek2x <zlorenzo@mcm.edu.ph>
2022-08-17 20:46:00 +0800

    Added files

---
+++ Ownership.java
@@ -0,0 +1,15 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Object Oriented Programming (Paired)
+ */
+
+public class Ownership {
+    private static String name = "Lorenzo, Zazheska D.";
+    private static String program = "CCIS - CS 2nd Year";
+    private static String section = "A221 - Object Oriented Programming (Paired)";
+
+    public static void Introduction() {
+        System.out.println("\n" + name + "\n" + program + "\n" + section + "\n");
+    }
+}
---
+++ Practice-Sets-Part-1/Greetings.java
@@ -0,0 +1,22 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Object Oriented Programming (Paired)
+ */
+
+public class Greetings {
+    public static void main(String[] args) {
+        Ownership.Introduction();
+
+        String greeting = """
+
+                Hello, nice to meet you!
+                I am Zazheska Lorenzo, a student
+                of Mapua Malayan Colleges Mindanao
+                studying computer science.
+                Pleased to meet you!
+
+                """;
+        System.out.print(greeting);
+    }
+}
