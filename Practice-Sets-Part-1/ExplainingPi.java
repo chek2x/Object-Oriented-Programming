2b807f3854e59647a7ad808314a20567ef37fff6
chek2x <zlorenzo@mcm.edu.ph>
2022-08-17 21:01:56 +0800

    Modified ExplainingPi.java to ExplainingPi2.java

--- Practice-Sets-Part-1/ExplainingPi.java
+++
@@ -1,8 +0,0 @@
-public class ExplainingPi {
-    public static void main(String[] args) {
-        System.out.println("Pi represents the ratio between");
-        System.out.println("the diameter and circumference");
-        System.out.println("of a circle");
-        System.out.println(Math.PI);
-    }
-}
---
+++ Practice-Sets-Part-1/ExplainingPi2.java
@@ -0,0 +1,23 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Object Oriented Programming (Paired)
+ */
+
+public class ExplainingPi2 {
+    public static void main(String[] args) {
+        System.out.println("Pi represents the ratio between");
+        System.out.println("the diameter and circumference");
+        System.out.println("of a circle");
+        System.out.println(Math.PI);
+
+        System.out.print("""
+                
+                This is a revision to conform to
+                what is to be learned from the activity.
+                The class name was renamed from
+                'ExplainingPi' to 'ExplainingPi2'
+                
+                """;);
+    }
+}
