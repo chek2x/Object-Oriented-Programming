89d9bf2e00667302b6689cf4ba0520d75b4e3440
chek2x <zlorenzo@mcm.edu.ph>
2022-08-30 10:01:24 +0800

    Modified TimeConverter.java

--- Assessment-Set1/TimeConverter.java
+++ Assessment-Set1/TimeConverter.java
@@ -1,3 +1,17 @@
-public class TimeConverter {
+import javax.swing.*;
 
+public class TimeConverter {
+    public static void main(String[] args) {
+        int sec = Integer.parseInt(JOptionPane.showInputDialog(null, "Enter amount of seconds\t: "));
+        convert(sec);
+    }
+
+    public static void convert(int sec) {
+        int hrs = sec / 3600;
+        int mns = (sec % 3600) / 60;
+        sec = (sec % 3600) % 60;
+
+        String str = hrs + " hours, " + mns + " minutes, and " + sec + " seconds.";
+        JOptionPane.showMessageDialog(null, str);
+    }
 }
