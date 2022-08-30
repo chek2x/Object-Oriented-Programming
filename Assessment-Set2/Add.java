b6dc78ed05f66531f6d6da233b37467fc8a378b0
chek2x <zlorenzo@mcm.edu.ph>
2022-08-31 02:00:11 +0800

    Added and modified multiple files

---
+++ Assessment-Set2/Add.java
@@ -0,0 +1,27 @@
+/**
+ * @author Lorenzo, Zazheska D.
+ * @param CCIS - CS 2nd Year
+ * @param A221 - Object Oriented Programming (Paired)
+ */
+
+public class Add {
+    public static void main(String[] args) {
+
+    }
+
+    public static void add(int in1, double in2) {
+        System.out.print("Int " + in1 + " + Double " + in2 + " = " + (in1 + in2));
+    }
+
+    public static void add(double in1, int in2) {
+        System.out.print("Double " + in1 + " + Int " + in2 + " = " + (in1 + in2));
+    }
+
+    public static void add(int in1, int in2) {
+        System.out.print("Int " + in1 + " + Int " + in2 + " = " + (in1 + in2));
+    }
+
+    public static void add(double in1, double in2) {
+        System.out.print("Double " + in1 + " + Double " + in2 + " = " + (in1 + in2));
+    }
+}
