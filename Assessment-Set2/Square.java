10a2157f522ce0881ff8c0011f6c12b4a543ce75
chek2x <zlorenzo@mcm.edu.ph>
2022-09-01 20:24:30 +0800

    Modified multiple files

--- Assessment-Set2/Square.java
+++ Assessment-Set2/Square.java
@@ -1,5 +1,25 @@
+import javax.swing.JOptionPane;
+
 public class Square {
     public static void main(String[] args) {
+        Ownership.Introduction();
 
+        int intValue;
+        double input = Double.parseDouble(
+                JOptionPane.showInputDialog(null, "Enter number: ", "Square", JOptionPane.QUESTION_MESSAGE));
+        if (input % 1 == 0) {
+            intValue = (int) input;
+            JOptionPane.showMessageDialog(null, square(intValue), "Result", JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(null, square(input), "Result", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    public static int square(int x) {
+        return x * x;
+    }
+
+    public static double square(double x) {
+        return Math.pow(x, 2);
     }
 }
