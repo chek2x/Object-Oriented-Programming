27b663d2bdf1b131da4372730aa058901b5248cf
chek2x <zlorenzo@mcm.edu.ph>
2022-09-01 20:27:43 +0800

    Modified Add.java

--- Assessment-Set2/Add.java
+++ Assessment-Set2/Add.java
@@ -6,22 +6,29 @@
 
 public class Add {
     public static void main(String[] args) {
+        Ownership.Introduction();
 
+        add(1, 2.5);
+        add(2.5, 1);
+        add(1, 2);
+        add(1.5, 2.5);
+
+        System.out.println();
     }
 
     public static void add(int in1, double in2) {
-        System.out.print("Int " + in1 + " + Double " + in2 + " = " + (in1 + in2));
+        System.out.println("Int " + in1 + " + Double " + in2 + " = " + (in1 + in2));
     }
 
     public static void add(double in1, int in2) {
-        System.out.print("Double " + in1 + " + Int " + in2 + " = " + (in1 + in2));
+        System.out.println("Double " + in1 + " + Int " + in2 + " = " + (in1 + in2));
     }
 
     public static void add(int in1, int in2) {
-        System.out.print("Int " + in1 + " + Int " + in2 + " = " + (in1 + in2));
+        System.out.println("Int " + in1 + " + Int " + in2 + " = " + (in1 + in2));
     }
 
     public static void add(double in1, double in2) {
-        System.out.print("Double " + in1 + " + Double " + in2 + " = " + (in1 + in2));
+        System.out.println("Double " + in1 + " + Double " + in2 + " = " + (in1 + in2));
     }
 }
